8b8c82698d19b301ba285d4ebf34d7a257ea1375
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-09-25 21:56:39 +0300

    tallennus toimii

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
 import { FlatList, SafeAreaView, StyleSheet, Text } from 'react-native'
 import Constants from 'expo-constants'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Row from './components/Row'
 import uuid from 'react-native-uuid'
 import Add from './components/Add'
+import Asyncstorage from '@react-native-async-storage/async-storage'
+
+const STORAGE_KEY = '@items_key'
 
 export default function App() {
   const [data, setData] = useState([])
   const [selectedId, setSelectedId] = useState(null)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  useEffect(() => {
+    storeData(data)
+  }, [data])
   
   const add = useCallback((name) => {
     const newItem = {
@@ -22,6 +33,28 @@ export default function App() {
   const select = useCallback((id) => {
     setSelectedId(id)
   }, [])
+
+  const getData = async () => {
+    try {
+      const value = await Asyncstorage.getItem(STORAGE_KEY)
+      const json = JSON.parse(value)
+      if (json === null) {
+        json = []
+      }
+      setData(json)
+    } catch (ex) {
+      console.error(ex)
+    }
+  }
+
+  const storeData = async (value) => {
+    try {
+      const json = JSON.stringify(value)
+      await Asyncstorage.setItem(STORAGE_KEY, json)
+    } catch (ex) {
+      console.error(ex)
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
