11f26b4d34e0ad53d3fbf6a336f36bc55afaf457
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-10-29 19:12:35 +0200

    testailua

--- apikeys.js
+++ apikeys.js
@@ -1,6 +1,8 @@
-export const FIRESTORE_APIKEY = ''
-export const FIRESTORE_AUTHDOMAIN = ''
-export const FIRESTORE_PROJECTID = ''
-export const FIRESTORE_STORAGEBUCKET = ''
-export const FIRESTORE_MESSAGINGSENDERID = ''
-export const FIRESTORE_APPID = ''
+export const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
