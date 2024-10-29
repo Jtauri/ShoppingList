17a65490a5f5dc6255292f2707d71b85315b705d
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-10-29 19:10:12 +0200

    eihän tää toimi ollenkaan

--- firebase/config.js
+++ firebase/config.js
@@ -1,14 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, addDoc, serverTimestamp, query, onSnapshot, orderBy } from 'firebase/firestore'
-import { FIRESTORE_APIKEY, FIRESTORE_AUTHDOMAIN, FIRESTORE_PROJECTID, FIRESTORE_STORAGEBUCKET, FIRESTORE_MESSAGINGSENDERID, FIRESTORE_APPID } from './apikeys'
-const firebaseConfig = {
-    apiKey: FIRESTORE_APIKEY,
-    authDomain: FIRESTORE_AUTHDOMAIN,
-    projectId: FIRESTORE_PROJECTID,
-    storageBucket: FIRESTORE_STORAGEBUCKET,
-    messagingSenderId: FIRESTORE_MESSAGINGSENDERID,
-    appId: FIRESTORE_APPID
-  }
+import { firebaseConfig } from './apikeys'
 
 initializeApp(firebaseConfig)
 
